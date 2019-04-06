f07f17d28c87979a3b5d35e5778b4fbd73fc1bc7
Arunabh Pandey <arunabhp75@gmail.com>
2019-04-07 00:19:50 +0530

    added list items from data.json

---
+++ src/app/app-component/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faTimes);
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent implements OnInit{
+  title = 'Angular App';
+  theList: object[];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() : void {
+    this.http.get<object[]>('../assets/data.json').subscribe(data => {
+      //console.log(data);
+      this.theList = data;
+    })
+  }
+
+}
